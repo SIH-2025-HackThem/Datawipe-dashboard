6bf9c42c67221855857cf8c03ed912b3de2e61a3
Vinitj088 <vinitjain088@gmail.com>
2025-09-18 10:34:28 +0530

    updated name to Swiftwipe

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { GeistMono } from "geist/font/mono"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "DataWipe",
+  title: "SwiftWipe",
   description: "Wipe your data with complete ensureness"
 }
 
